fe6f3f46fa8d239e181783ddbee4d1827dc07ddb
SeonAh Kim <111446760+kokokim@users.noreply.github.com>
2024-05-20 23:43:21 +0900

    [Bronze II] Title: 소수 찾기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,16 @@
+n=int(input())
+arr=list(map(int, input().split()))
+
+s=True
+num=0
+for i in arr:
+    if i==0 or i==1:
+        continue
+    for j in range(2, i):
+        if i%j==0:
+            s=False
+    if s==True:
+        num+=1
+    s=True
+
+print(num)
